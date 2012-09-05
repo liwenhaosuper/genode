f10127dcdd03737c8435a526ce763ef996c94796
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2012-09-05 14:28:37 +0200

    Fiasco.OC: update to recent version (svn rev. 40)

--- base-foc/src/kernel/panda/target.mk
+++ base-foc/src/kernel/panda/target.mk
@@ -1,4 +1,5 @@
 REQUIRES      = platform_panda
-KERNEL_CONFIG = $(REP_DIR)/config/panda.kernel
+FIASCO_DIR    = $(REP_DIR)/contrib/kernel/fiasco
+KERNEL_CONFIG = $(FIASCO_DIR)/src/templates/globalconfig.out.arm-omap4-panda
 
 -include $(PRG_DIR)/../target.inc
