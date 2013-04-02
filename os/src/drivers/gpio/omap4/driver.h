81b684f6efe36732b93636e44a0e89d7fcc214ba
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2013-04-02 13:46:15 +0200

    Omap4 sd_card driver: fix recursive usleep loop
    This commit removes an endless loop, that occurred when the sd_card driver
    called usleep, or msleep on its Timer_delayer object. Fixes #705
    
    Also fixes the same problem for the Omap4 GPIO driver.

--- os/src/drivers/gpio/omap4/driver.h
+++ os/src/drivers/gpio/omap4/driver.h
@@ -48,21 +48,7 @@ class Gpio::Driver
 
 		struct Timer_delayer : Timer::Connection, Mmio::Delayer
 		{
-			/**
-			 * Implementation of 'Delayer' interface
-			 */
-			void usleep(unsigned us)
-			{
-				/* polling */
-				if (us == 0)
-					return;
-
-				unsigned ms = us / 1000;
-				if (ms == 0)
-					ms = 1;
-
-				Timer::Connection::msleep(ms);
-			}
+			void usleep(unsigned us) { Timer::Connection::usleep(us);
 		} _delayer;
 
 		/* memory map */
